44bd1610b18d7595b73fb61d71acf71d1d77bbbc
chava <chava@chavas-Mac.local>
2015-06-13 07:12:39 -0700

    Initial Commit

---
+++ ios-Table/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-Table
+//
+//  Created by chava on 6/13/15.
+//  Copyright (c) 2015 fuentess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios-Table/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios-Table
+//
+//  Created by chava on 6/13/15.
+//  Copyright (c) 2015 fuentess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
